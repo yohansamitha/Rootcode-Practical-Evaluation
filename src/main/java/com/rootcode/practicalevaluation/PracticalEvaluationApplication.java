2d62cdf48268cfd2c3ed648d237a00120005e5f2
yohansamitha123 <65862807+yohansamitha@users.noreply.github.com>
2025-04-23 18:13:08 +0530

    Add initial implementation of library management system with user authentication and book borrowing features

---
+++ src/main/java/com/rootcode/practicalevaluation/PracticalEvaluationApplication.java
@@ -0,0 +1,13 @@
+package com.rootcode.practicalevaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalEvaluationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalEvaluationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rootcode/practicalevaluation/PracticalEvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rootcode.practicalevaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalEvaluationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
